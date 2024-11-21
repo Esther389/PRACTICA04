379f4f45c06af92804cc30a64266db6cf021a1b7
unknown <estherquispe389@gmail.com>
2024-11-21 12:57:27 -0400

    prac4

---
+++ resources/views/Fronted/home/index.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+
+    </header><html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="ESTHER QUISPE PACCI">
+    <meta name="description" content="musica urbana">
+    <meta name="keywords" content="musica, urbano, talento, reggaeton, rap, remix, flow, dj, estilo, cultura, trap, ritmo, perreo, exito">
+    <meta name="viewport" content="width=deice, initial-scale=1, maximum-scale=1">
+    <title>Musica Urbana</title>
+    <link hrel="{{asset('estilos.css')}}" rel="stylesheet=">
+    <link hrel="stylesheet" href="es/estilo.css">
+      
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="index.html">Inicio</a>
+            <a href="nosotros.html" target="_blank">Nosotros</a>
+            <a href="#B1">Artistas</a>
+            <a href="#A1">Contactos</a>
+        </nav>
+        <section  class="textos">
+            <h1>MY URBAN MUSIC</h1>
+            <h2>Mi arma es el micrófono y mis rimas son las balas.</h2>
+        </section>
+        <div class="ola"style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" 
+            style="height: 100%; width: 100%;">
+            <path d="M0.00,49.93 C150.00,149.82 349.20,-49.93 500.00,49.93 L500.00,149.82 L0.00,149.82 Z" 
+            style="stroke: none; fill: #010101 ;"></path>
+        </svg></div>
+    <main>
+        <div class="contenido">
+            <div class="articulos">
+                <div class="secciones">
+                    <div class="Nombre">
+                        <h2 id="MU1">MUSICA URBANA</h2>
+                        <br>
+                        <p>El término de música urbana hace referencia a todos aquellos estilos musicales que nacen durante los años 80. Hablamos, ni más ni menos, que del auge del soul, el hip hop y la música R & B. Durante sus inicios, este concepto se utiliza de manera despectiva para mencionar las comunidades afroamericanas que dieron origen a todos estos ritmos.
+                            La incorporación de nuevos artistas y la mezcla de estilos ha hecho que la música urbana siga creciendo y evolucionando hasta lo que conocemos hoy en día. Un movimiento que reivindica mensajes, estilos propios y visibilidad de los barrios más desfavorecidos.              
+                            La década de los 90 ha sido clave para el éxito tan rotundo de este nuevo concepto y combinación de estilos. Incluso, en los últimos años, se ha llegado a convertir en un auténtico fenómeno de masas.
+                        </p>
+                    </div>
+                    <br>
+                    br
+                    <div class="link">
+                        <h3>Historia de la musica urbana:  <a href="https://es.wikipedia.org/wiki/Urban" target="_blank">click aqui</a></h3>
+                    </div>
+                </div>
+            </div>       
+            <div class="menu">
+                <img src="{{asset('frontend/img/imgMU.jpg')}}" 9alt="" height="300px">
+            </div>
+        </div>
+        <section class="portafolio">
+            <div class="contenedor">
+                <h2 class="titulo" id="B1">ARTISTAS</h2>
+                <div class="galeria">
+                    <div class="imagenes">
+                        <img src="{{asset('frontend/img/imgD.jpg')}}" alt="https://daddyyankee.com/">
+
+                        <div class="hover-galeria" href="#N1">
+                            <a href="daddyyankee.html"><img src="{{asset('frontend/img/logo1.jpg')}}"></a>
+                        </div>
+                    </div>
+                    <div class="imagenes">
+                        <img src="{{asset('frontend/img/imgN.jpg')}}" alt="">
+                        <div class="hover-galeria">
+                            <a href="nickyjam.html"><img src="{{asset('frontend/img/logo2.jpg')}}" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="imagenes">
+                        <img src="{{asset('frontend/img/imgA.jpg')}}" alt="">
+                        <div class="hover-galeria">
+                            <a href="anuel.html"><img src="{{asset('frontend/img/logo3.jpg')}}" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="imagenes">
+                        <img src="{{asset('frontend/img/imgM.jpg')}}" alt="">
+                        <div class="hover-galeria">
+                            <a href="maluma.html"><img src="{{asset('frontend/img/logo4.jpg')}}" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="imagenes">
+                        <img src="{{asset('frontend/img/imgP.jpg')}}" alt="">
+                        <div class="hover-galeria">
+                            <a href="planb.html"><img src="{{asset('frontend/img/logo5.jpg')}}" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="imagenes">
+                        <img src="{{asset('frontend/img/imgCN.jpg')}}" alt="">
+                        <div class="hover-galeria">
+                            <a href="chino&nacho.html"><img src="{{asset('frontend/img/logo7.jpg')}}" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="imagenes">
+                        <img src="{{asset('frontend/img/imgWY.jpg')}}" alt="">
+                        <div class="hover-galeria">
+                            <a href="wisin&yandel.html"><img src="{{asset('frontend/img/logo8.jpg')}}" alt=""></a>
+                        </div>
+                    </div>
+                
+                </div>
+                <div class="masArtistas">
+                    <a href="https://www.letras.com/estilos/reggaeton/artistas.html" target="_blank">mas artitas</a>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div class="contactos">
+            <h5>Para recibir las noticias y todo sobre nuestros contenidos, comunicate con nosotros.    <a href="Nosotros.html">Suscribete...</a></h5>
+        </div>
+        <br>
+        <div class="contenedor-footer" id="A1">
+            <div class="content-foo">
+                <div class="whatsapp">
+                    <a href="https://www.whatsapp.com/" target="_blank">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="whatsapp" class="svg-inline--fa fa-whatsapp fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path></svg>
+                    </a>
+                 </div>
+                <h4>whatsapp</h4>
+            </div>
+            <div class="content-foo">
+                <a href="https://www.facebook.com/groups/1628247284125173" target="_blank">
+                    <img alt="Siguenos en Facebook" src="https://lh6.googleusercontent.com/-CYt37hfDnQ8/T3nNydojf_I/AAAAAAAAAr0/P5OtlZxV4rk/s32/facebook32.png" width=30 height=30  />
+                </a>
+                <h4>facebook</h4>
+            </div>
+            <div class="content-foo">
+                <a href="https://twitter.com/RegRomantico" target="_blank">
+                    <img src="https://cdn-icons-png.flaticon.com/512/3670/3670151.png" alt="gorjeo" title="gorjeo" width="30" height="30">
+                </a>
+                <h4>twitter</h4>
+            </div>
+        </div>
+        <h2 class="titulo-final">© Pagina creado por el estudiante Quispe Pacci Esther </h2>
+    </footer>
+</body>
+</html>
